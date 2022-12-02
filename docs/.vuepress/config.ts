8e722feb30ea9c53bec11df8b7b422333908a7f6
sword4869 <1084662708@qq.com>
2022-12-02 12:19:50 +0800

    token

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   base: '/learn-python/',
   themeConfig: {
     // if your docs are in a different repo from your main project:
-    docsRepo: "sword4869/vuepress-starter",
+    docsRepo: "sword4869/learn_python",
     // if your docs are in a specific branch (defaults to 'master'):
     docsBranch: "main",
     // if your docs are not at the root of the repo:
@@ -12,6 +12,18 @@ export default defineConfig({
     // defaults to false, set to true to enable
     editLinks: true,
     // custom text for edit link. Defaults to "Edit this page"
-    editLinkText: 'Help us improve this page!'
-    }
+    editLinkText: 'Help us improve this page!',
+    
+    sidebar: [
+      {
+        title: 'virtual environment',
+        children: [
+          ['/virtual environment/conda.md','conda.md'],
+          ['/virtual environment/pip.md','pip.md'],
+          ['/virtual environment/requirements.md','requirements.md'],
+          ['/virtual environment/virtualenv.md','virtualenv.md'],
+        ],
+      }
+    ]
+  },
 });
