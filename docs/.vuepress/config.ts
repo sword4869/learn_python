9befb14753e467b3422cb2d8c6a082658afc72cc
sword4869 <1084662708@qq.com>
2022-12-03 22:24:29 +0800

    fuck!

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -26,7 +26,72 @@ export default defineConfig({
             ],
           ],
         },
-        
+        {
+          title: "virtual environment",
+          children: [
+            ["/virtual environment/conda.md", "conda.md"],
+            ["/virtual environment/pip.md", "pip.md"],
+            ["/virtual environment/requirements.md", "requirements.md"],
+            ["/virtual environment/virtualenv.md", "virtualenv.md"],
+          ],
+        },
+        { title: "压缩", children: [["/压缩/pickle.md", "pickle.md"]] },
+        { title: "命令行", children: [["/命令行/命令行.md", "命令行.md"]] },
+        { title: "图像", children: [["/图像/ffmpy.md", "ffmpy.md"]] },
+        { title: "声音处理", children: [["/声音处理/pydub.md", "pydub.md"]] },
+        {
+          title: "控制台效果",
+          children: [
+            ["/控制台效果/color.md", "color.md"],
+            ["/控制台效果/print.md", "print.md"],
+          ],
+        },
+        {
+          title: "路径",
+          children: [
+            ["/路径/os.md", "os.md"],
+            ["/路径/other.md", "other.md"],
+          ],
+        },
+        {
+          title: "配置",
+          children: [
+            {
+              title: "读取命令行",
+              children: [
+                ["/配置/读取命令行/argparse.md", "argparse.md"],
+                ["/配置/读取命令行/sys and getopt.md", "sys and getopt.md"],
+              ],
+            },
+            {
+              title: "配置文件",
+              children: [
+                ["/配置/配置文件/regex.md", "regex.md"],
+                {
+                  title: "configargparse",
+                  children: [
+                    [
+                      "/配置/配置文件/configargparse/configargparse.md",
+                      "configargparse.md",
+                    ],
+                  ],
+                },
+                {
+                  title: "ini",
+                  children: [["/配置/配置文件/ini/ini.md", "ini.md"]],
+                },
+                {
+                  title: "json",
+                  children: [["/配置/配置文件/json/json.md", "json.md"]],
+                },
+                {
+                  title: "yaml",
+                  children: [["/配置/配置文件/yaml/yaml.md", "yaml.md"]],
+                },
+              ],
+            },
+          ],
+        },
       ],
     },
   },
